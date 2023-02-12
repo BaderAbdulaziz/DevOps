ebb0d09ad862b971d4c99adbe7694d2cc45ec6ff
BaderAbdulaziz <124074937+BaderAbdulaziz@users.noreply.github.com>
2023-02-12 22:09:31 +0300

    Add files via upload

---
+++ JenkinsWithGit.java
@@ -0,0 +1,12 @@
+public class JenkinsWithGit {
+    public static void main(String[] args) {
+        int charPerLine=0;
+        for (int i = 33; i < 256; i++, charPerLine++)
+    {
+        system.out.print((char)i+((charPerLine%10==0)?"\n":"\t"));
+        system.out.print();
+    }
+
+    }
+
+}
